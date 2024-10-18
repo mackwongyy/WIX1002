b2e59cd04b71ceef10b1c0b47b8d4feb32f6e58b
mackwongyy <95009744+mackwongyy@users.noreply.github.com>
2024-10-18 15:43:11 +0800

    Add files via upload

---
+++ L1Q3.java
@@ -0,0 +1,15 @@
+//WONG YOONG YEE, S2118482, UM AY2023/2024 WIX1002
+
+public class L1Q3 {
+    public static void main(String args[]) {
+        System.out.println("*********\t   ***   \t    *    \t    *    ");
+        System.out.println("*       *\t *     * \t   ***   \t   * *   ");
+        System.out.println("*       *\t*       *\t  *****  \t  *   *  ");
+        System.out.println("*       *\t*       *\t    *    \t *     * ");
+        System.out.println("*       *\t*       *\t    *    \t*       *");
+        System.out.println("*       *\t*       *\t    *    \t *     * ");
+        System.out.println("*       *\t*       *\t    *    \t  *   *  ");
+        System.out.println("*       *\t *     * \t    *    \t   * *   ");
+        System.out.println("*********\t   ***   \t    *    \t    *    ");
+    }
+}
